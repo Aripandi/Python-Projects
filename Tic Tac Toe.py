ce382d9baa8d098fe48049ad11c85dfb3d885fa3
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-01-30 06:55:26 +0530

    Update Tic Tac Toe.py

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -58,3 +58,17 @@ def full_board_check(board):
         if space_check(board, i):
             return False
     return True
+
+def player_choice(board):
+    position = 0
+    
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9): '))
+        
+    return positiondef player_choice(board):
+    position = 0
+    
+    while position not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose your next position: (1-9): '))
+        
+    return position
