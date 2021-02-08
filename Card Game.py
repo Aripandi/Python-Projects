eb95325ebff68b1f4b378df3c1ce67588f37ebe5
Developer_Hub <69900642+Aripandi@users.noreply.github.com>
2021-02-08 22:17:38 +0530

    Update Card Game.py

--- Card Game.py
+++ Card Game.py
@@ -10,3 +10,13 @@ rank = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 
         'Queen', 'King', 'Ace')
 values = {"Two":2, "Three":3,"Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10,
          "Jack":11, "Queen":12, "Kimg":13, "Ace":14}
+
+class card():
+    
+    def __init__(self,suits,rank):
+        self.suits = suits
+        self.rank = rank
+        self.value = values[rank]
+        
+    def __str__(self):
+        return self.rank + " of " +self.suits
